d16804869a2e470a4928726ae06bb77e89f002e9
Khushboo-tiwari58 <113780060+Khushboo-tiwari58@users.noreply.github.com>
2024-04-27 19:07:32 +0530

    Update login.php

--- login.php
+++ login.php
@@ -29,7 +29,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <?php require 'components/_nav.php'?>
+    <?php require '_nav.php'?>
     <?php
     if($login){
       echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
